e43852bae9e4e8370136b64f296563adc313358c
pramodjha <jha.pramod234@gmail.com>
2021-04-09 09:11:07 +0530

    Uploading project

---
+++ adminpanel/admin/home.php
@@ -0,0 +1,72 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['admin']['adminnakalogin']) == true) header("location:index.php");
+
+
+ ?>
+<?php include("../../conn.php"); ?>
+<!-- MAO NI ANG HEADER -->
+<?php include("includes/header.php"); ?>      
+
+<!-- UI THEME DIRI -->
+<?php include("includes/ui-theme.php"); ?>
+
+<div class="app-main">
+<!-- sidebar diri  -->
+<?php include("includes/sidebar.php"); ?>
+
+
+
+<!--  -->
+<?php 
+   @$page = $_GET['page'];
+
+
+   if($page != '')
+   {
+     if($page == "add-course")
+     {
+     include("pages/add-course.php");
+     }
+     else if($page == "manage-course")
+     {
+     	include("pages/manage-course.php");
+     }
+     else if($page == "manage-exam")
+     {
+      include("pages/manage-exam.php");
+     }
+     else if($page == "manage-examinee")
+     {
+      include("pages/manage-examinee.php");
+     }
+     else if($page == "ranking-exam")
+     {
+      include("pages/ranking-exam.php");
+     }
+     else if($page == "feedbacks")
+     {
+      include("pages/feedbacks.php");
+     }
+     else if($page == "examinee-result")
+     {
+      include("pages/examinee-result.php");
+     }
+
+       
+   }
+   // 
+   else
+   {
+     include("pages/home.php"); 
+   }
+
+
+ ?> 
+
+
+
+<?php include("includes/footer.php"); ?>
+
+<?php include("includes/modals.php"); ?>
